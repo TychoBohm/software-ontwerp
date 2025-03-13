4896d8a3289f6dc5be7630ec09a304bbd8c3fd78
dylan de beer <2090420@talnet.nl>
2025-03-13 13:07:46 +0100

    nav toegevoegd test

--- index.php
+++ index.php
@@ -10,10 +10,18 @@
 </head>
 
 <body>
+    <nav class="nav--bar">
+        <ul class="nav--ul">
+            <li class=""><a href="index.php">Home</a></li>
+            <li class=""><a href="about.php">About</a></li>
+            <li class=""><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
     <p>hello world</p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+
 </body>
 
 </html>
