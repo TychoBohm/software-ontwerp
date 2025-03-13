efe28f10956491b9eaaac4ae171df4c7dd513ad8
Tycho Bohm <2189905@talnet.nl>
2025-03-13 13:26:25 +0100

    opzetting project

--- index.php
+++ index.php
@@ -5,23 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tycho Böhm</title>
+    <link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
     <nav class="nav--bar">
+        <p>Naam</p>
         <ul class="nav--ul">
-            <li class=""><a href="index.php">Home</a></li>
-            <li class=""><a href="about.php">About</a></li>
-            <li class=""><a href="contact.php">Contact</a></li>
+            <li class="nav--li"><a href="index.php">Home</a></li>
+            <li class="nav--li"><a href="about.php">About</a></li>
+            <li class="nav--li"><a href="contact.php">Contact</a></li>
         </ul>
     </nav>
-    <p>hello world</p>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    <section>
+        <p>hello world</p>
+    </section>
+    <footer>
+
+    </footer>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-
 </body>
 
 </html>
